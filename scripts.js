0301eb83789fe1b9376003ff141c453eb4ff2a7d
AnuBis7Star <enriquenascutiualin1@gmail.com>
2025-02-03 10:38:20 +0100

    Random choice implemented.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log("Hello everubody");
+
+function getComputerChoice() {
+    let choice = 0; // 0 = rock, 1 = paper, 2 = scissors
+    let randomchoice = Math.random();
+    
+    if (randomchoice <= 0.33) {
+        choice = 0; // Rock
+        return "Rock";
+    } else if (randomchoice > 0.33 && randomchoice <= 0.66) {
+        choice = 1; // Paper
+        return "Paper";
+    } else {
+        choice = 2; // Scissors
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
